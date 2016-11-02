d7872a8ebf921a258769c0cd84418a3e3b075951
super-s <wsyjob@163.com>
2016-11-02 17:57:30 +0800

    修改icon，hey

--- js/GraphicWork.js
+++ js/GraphicWork.js
@@ -1,27 +1,38 @@
 ;$(document).ready(function (){
 	var num = 0;
 	var scrollTop = 0;
-	
-	
+
+
 	$(window).scroll(function(ev){
 		var height = 18928;	//滚动条可以滚动的最大高度
 		scrollTop = $(window).scrollTop();	//滚动条滚动的高度
 		//console.log(scrollTop)
 		num = scrollTop/height*100;			//滚动条滚动的百分比
-		$('.line-inner').css('width',num+"%")		
+		$('.line-inner').css('width',num+"%")
 		mouseWheel(document,upFn,downFn);
 	});
-	
+
 	var chars = document.getElementsByClassName("chars");
-	
-	
+
+
 	for(let i = 0 ; i < chars.lenght; i++){
 		chars[i].stage = false;	//所有的都为隐藏状态
 	};
 	chars[0].stage = true;
-	
+
+	/*if(num < 3500){
+		chars[0].stage = true;
+	}
+	if(num >= 3500 && num <= 8300){
+		chars[1].stage = true;
+	}
+	if(num <= 8300){
+		chars[2].stage = true;
+	}*/
+
+
 	function downFn(){
-		if(scrollTop >= 3500 && scrollTop <= 4500){
+		if(scrollTop >= 3500 ){
 			if(chars[0].stage){
 				$('.chars').eq(0).animate({
 					top: -250,
@@ -34,9 +45,8 @@
 				chars[0].stage = false;
 				chars[1].stage = true;
 			}
-			
 		}
-		if(scrollTop >= 8300 && scrollTop <= 9300 ){
+		if(scrollTop >= 8300 ){
 			if(chars[1].stage){
 				$('.chars').eq(1).animate({
 					top: -250,
@@ -48,11 +58,11 @@
 				},1200)
 				chars[1].stage = false;
 				chars[2].stage = true;
-			}		
+			}
 		}
 	}
 	function upFn(){
-		if(scrollTop >= 3500 && scrollTop <= 4500){
+		if(scrollTop <= 4500){
 			if(chars[1].stage){
 				$('.chars').eq(0).animate({
 					top: -82.5,
@@ -66,7 +76,7 @@
 				chars[1].stage = false;
 			}
 		}
-		if(scrollTop >= 8300 && scrollTop <= 9300){
+		if(scrollTop <= 9300){
 			if(chars[2].stage){
 				$('.chars').eq(1).animate({
 					top: -82.5,
@@ -81,9 +91,8 @@
 			}
 		}
 	}
-	
-	
-	
+
+
 	function mouseWheel(element,upFn,downFn){
         element.onmousewheel = wheelFn;
         if( element.addEventListener ){
@@ -105,15 +114,30 @@
             //ev.preventDefault();
         }
     }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+	$('.toTop').on('click',function (){
+		console.log(123);
+			$('.chars').eq(2).animate({
+				top: 250,
+				opacity: 0
+			},1200)
+			$('.chars').eq(1).animate({
+				top: 250,
+				opacity: 0
+			},1200)
+			$('.chars').eq(0).animate({
+				top: -82.5,
+				opacity: 1
+			},1200)
+			chars[2].stage = false;
+			chars[0].stage = true;
+	})
+
+
+
+
+
+
+
 });
